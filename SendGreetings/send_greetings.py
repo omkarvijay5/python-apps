abd0942c2f4696589b8449d3b12f34187ee1db78
omkarvijay5 <omkarvijay5@gmail.com>
2015-12-31 11:48:16 +0530

    email validation command line

---
+++ SendGreetings/send_greetings.py
@@ -0,0 +1,22 @@
+import re
+
+EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$")
+
+
+def get_email():
+    while 1:
+        email = raw_input("Please Enter your email:")
+        if not EMAIL_REGEX.match(email):
+            print "Please enter proper email"
+        else:
+            break
+    return email
+
+
+def main():
+    print "Welcome to send_greetings"
+    email = get_email()
+
+
+if __name__ == '__main__':
+    main()
