352774af3289e24d16c675df6788fb0a60a0b301
omkarvijay5 <omkarvijay5@gmail.com>
2015-12-31 11:53:00 +0530

    updated regex

--- SendGreetings/send_greetings.py
+++ SendGreetings/send_greetings.py
@@ -1,6 +1,6 @@
 import re
 
-EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+.[a-zA-Z]{2,6}$")
+EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$")
 
 
 def get_email():
