2168294e669481a2729ce01d184ba62a17b67e25
omkarvijay5 <omkarvijay5@gmail.com>
2015-12-31 13:47:16 +0530

    used settings.txt for email settings

--- SendGreetings/send_greetings.py
+++ SendGreetings/send_greetings.py
@@ -1,12 +1,40 @@
 import re
+import smtplib
+from email.mime.text import MIMEText
 
 EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$")
+SERVER = 'smtp.gmail.com'
+PORT = 587
+# Dont forget to turn on https://www.google.com/settings/security/lesssecureapps for your gmail
 
 
 class Greeting(object):
 
-    def __init__(self, email):
-        self.from_email = email
+    def __init__(self):
+        self.message = self.get_message()
+        with open('settings.txt') as f:
+            self.settings = dict(line.rstrip().split(None, 1) for line in f)
+        self.message['subject'] = self.settings['subject']
+        # validate_settings(self.settings)
+
+    def get_message(self):
+        greeting_template = open('templates/new_year.txt', 'rb')
+        msg = MIMEText(greeting_template.read())
+        greeting_template.close()
+        return msg
+
+    def setup_smtp_server(self):
+        server = smtplib.SMTP(SERVER, PORT)
+        server.starttls()
+        from_email = self.settings['from_email']
+        password = self.settings['password']
+        server.login(from_email, password)
+        return server
+
+    def send_email(self):
+        server = self.setup_smtp_server()
+        server.sendmail(self.settings['from_email'], ['omkarvijay5@gmail.com'], self.message.as_string())
+        server.quit()
 
 
 def get_email():
@@ -20,9 +48,11 @@ def get_email():
 
 
 def main():
-    print "Welcome to send_greetings"
-    email = get_email()
-    greeting = Greeting(email)
+    print "Welcome to send_greetings........!!!!"
+    # email = get_email()
+    # message = get_message()
+    greeting = Greeting()
+    greeting.send_email()
 
 
 if __name__ == '__main__':
