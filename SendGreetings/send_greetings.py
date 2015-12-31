bd7f9280282b91da5db627104486270b8566f2b2
omkarvijay5 <omkarvijay5@gmail.com>
2015-12-31 11:56:26 +0530

    Greeting class

--- SendGreetings/send_greetings.py
+++ SendGreetings/send_greetings.py
@@ -3,6 +3,12 @@ import re
 EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$")
 
 
+class Greeting(object):
+
+    def __init__(self, email):
+        self.from_email = email
+
+
 def get_email():
     while 1:
         email = raw_input("Please Enter your email:")
@@ -16,6 +22,7 @@ def get_email():
 def main():
     print "Welcome to send_greetings"
     email = get_email()
+    greeting = Greeting(email)
 
 
 if __name__ == '__main__':
