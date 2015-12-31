08270027eade1da7500a16f39577fe68642d5260
omkarvijay5 <omkarvijay5@gmail.com>
2015-12-31 23:18:51 +0530

    csv send mail

--- SendGreetings/send_greetings.py
+++ SendGreetings/send_greetings.py
@@ -1,31 +1,40 @@
 import re
 import smtplib
+import csv
 from email.mime.text import MIMEText
 
+# Email regex validation
 EMAIL_REGEX = re.compile("^[a-zA-Z0-9._%-+]+@[a-zA-Z0-9._%-]+\.[a-zA-Z]{2,6}$")
+# smtp settings
 SERVER = 'smtp.gmail.com'
 PORT = 587
-# Dont forget to turn on https://www.google.com/settings/security/lesssecureapps for your gmail
+""" Dont forget to turn on 
+    https://www.google.com/settings/security/lesssecureapps for your gmail
+"""
 
 
 class Greeting(object):
 
     def __init__(self):
-        self.message = self.get_message()
         with open('settings.txt') as f:
             self.settings = dict(line.rstrip().split(None, 1) for line in f)
-        self.message['subject'] = self.settings['subject']
-        # validate_settings(self.settings)
 
-    def get_message(self):
+    def compose_message(self, person_details):
         greeting_template = open('templates/new_year.txt', 'rb')
-        msg = MIMEText(greeting_template.read())
+        first_name = person_details['first_name']
+        last_name = person_details['last_name']
+        message = greeting_template.read().format(first_name, last_name)
+        msg = MIMEText(message)
         greeting_template.close()
+        msg['Subject'] = self.settings['subject']
+        msg['From'] = self.settings['from_email']
+        msg['To'] = person_details['email']
         return msg
 
     def setup_smtp_server(self):
         server = smtplib.SMTP(SERVER, PORT)
         server.starttls()
+        server.ehlo()
         from_email = self.settings['from_email']
         password = self.settings['password']
         server.login(from_email, password)
@@ -33,7 +42,12 @@ class Greeting(object):
 
     def send_email(self):
         server = self.setup_smtp_server()
-        server.sendmail(self.settings['from_email'], ['omkarvijay5@gmail.com'], self.message.as_string())
+        contacts = open('contacts.csv', 'rb')
+        reader = csv.DictReader(contacts)
+        for person_details in reader:
+            to_email = person_details['email']
+            message = self.compose_message(person_details).as_string()
+            server.sendmail(self.settings['from_email'], [to_email], message)
         server.quit()
 
 
@@ -49,8 +63,7 @@ def get_email():
 
 def main():
     print "Welcome to send_greetings........!!!!"
-    # email = get_email()
-    # message = get_message()
+    print "Sending greetings .. . . ."
     greeting = Greeting()
     greeting.send_email()
 
